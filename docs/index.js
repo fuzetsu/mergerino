434161cc523ba7bf587b469f53dfde6ca2fb0c8f
Daniel Loomer <fuzetsu490@gmail.com>
2019-05-10 15:27:25 -0400

    0.0.1 initial souce commit

---
+++ docs/index.js
@@ -0,0 +1,64 @@
+// UNPKG
+import merge, { SUB, DEL } from 'https://unpkg.com/mergerino?module'
+
+// SOURCE
+// import merge, { SUB, DEL } from '../src/index.js'
+
+// ES5 DIST
+// import '../dist/mergerino.es5.min.js'
+// const merge = mergerino
+// const { DEL, SUB } = merge
+
+// ES6 DIST
+// import merge, { SUB, DEL } from '../dist/mergerino.min.js'
+
+DEL.toJSON = () => '(DEL)'
+SUB()._SUB.toJSON = () => '(SUB)'
+
+document.body.innerHTML = `
+<style>
+  #output {
+    background-color: #263238;
+    padding: 20px;
+    margin: 50px;
+    border-radius: 8px;
+    color: white;
+    box-shadow: 0 0 20px 1px rgba(0,0,0,0.5);
+  }
+  .string { color: #c3e88d; }
+  .comment { color: #546e7a; }
+  .value { color: #f77669; }
+  .braces { color: #fff; }
+</style>
+<pre id="output"><pre>
+`
+
+let state = {}
+
+const testMerge = patch => {
+  state = merge(state, patch)
+  console.log(state)
+  output.textContent +=
+    '\n\n\n// --> patched with ' +
+    JSON.stringify(typeof patch === 'function' ? patch.toString().replace(/\s+/g, ' ') : patch) +
+    '\n\n' +
+    JSON.stringify(state, null, 2)
+}
+
+testMerge({ count: 3 })
+testMerge({ count: SUB(x => x ** 3) })
+testMerge({ hello: 'world' })
+testMerge({ hello: DEL, coords: { x: 1, y: 2, z: 3 } })
+testMerge({ coords: { y: 10 } })
+testMerge({ hello: { amigo: 'carlos' } })
+testMerge({ hello: SUB({}) })
+testMerge(state => {
+  return { replaced: true, coords: state.coords }
+})
+
+output.innerHTML = output.innerHTML
+  .trim()
+  .replace(/("[^"]*")/g, `<span class="string">$1</span>`)
+  .replace(/(\/\/.+)/g, `<span class="comment">$1</span>`)
+  .replace(/(:\s*?)([^{,}]+)/g, `$1<span class="value">$2</span>`)
+  .replace(/[{}]/g, `<span class="braces">$&</span>`)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const _SUB = {}
+export const SUB = run => ({ _SUB, run })
+export const DEL = {}
+
+const assign = Object.assign || ((a, b) => (Object.keys(b).forEach(k => (a[k] = b[k])), a))
+
+const merge = (source, ...patches) => {
+  const isArr = Array.isArray(source)
+  let res = isArr ? source.slice() : assign({}, source)
+  for (const patch of patches) {
+    const type = typeof patch
+    if (patch && type === 'object') {
+      for (const k of Object.keys(patch)) {
+        const val = patch[k]
+        if (val == null || typeof val !== 'object' || Array.isArray(val)) res[k] = val
+        else if (val === DEL) isArr && !isNaN(k) ? res.splice(k, 1) : delete res[k]
+        else if (val._SUB === _SUB)
+          res[k] = typeof val.run === 'function' ? val.run(res[k]) : val.run
+        else if (typeof res[k] === 'object' && patch[k] !== res[k]) res[k] = merge(res[k], val)
+        else res[k] = val
+      }
+    } else if (type === 'function') res = patch(res)
+  }
+  return res
+}
+
+export default merge
