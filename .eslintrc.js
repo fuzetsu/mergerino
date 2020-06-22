3e2f6b02aad7550fa3367f6f65f839ea4b05df24
Daniel Loomer <fuzetsu@users.noreply.github.com>
2020-06-21 23:02:17 -0400

    add prettier and eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {},
+}
